255a6ce2601f8c12625b0599cc4708fb6916f565
Leelee Tian <tianlihang2013@gmail.com>
2018-05-09 23:18:14 +0800

    Complete guess-number

--- main/main.js
+++ main/main.js
@@ -1,6 +1,31 @@
-// Write your cade below:
-function main() {};
+'use strict'
 
-module.exports = {
-    main
+function split(string) {
+  let arr = string.match(/\d+/g).map((s) => s.split(''));
+  return (arr.length != 1)? {system: arr[0], userInput: arr[1]}
+  : {system: arr[0], userInput: []};
 }
+
+function print(counter) {
+  return`${counter.A}A${counter.B}B`;
+}
+
+function compareNum(numString){
+  let [a, b] = [0, 0];
+  if(numString.userInput !== [] && numString.userInput.length == numString.system.length){
+    for(var index in numString.system){
+      (numString.system[index] === numString.userInput[index])? a++
+      : (numString.userInput.includes(numString.system[index]))? b++ : null;
+    }
+  }
+  return {A: a, B: b};
+}
+
+function main(string) {
+  const numString = split(string);
+  var counter = compareNum(numString);
+  return print(counter);
+};
+
+
+module.exports = main;
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,30 @@
+'use strict'
+
 const main = require('../main/main');
 
 describe('main()', () => {
-
+  it('return 0A0B given empty input compared with 1234', () => {
+    let result = main('The system generates 1234 and user inputs .');
+    expect(result).toEqual('0A0B');
+  });
+  it('return 0A0B given 12 compared with 1234', () => {
+    let result = main('The system generates 1234 and user inputs 12.');
+    expect(result).toEqual('0A0B');
+  });
+  it('return 4A0B given 1234 compared with 1234', () => {
+    let result = main('The system generates 1234 and user inputs 1234.');
+    expect(result).toEqual('4A0B');
+  });
+  it('return 0A4B given 4321 compared with 1234', () => {
+    let result = main('The system generates 1234 and user inputs 4321.');
+    expect(result).toEqual('0A4B');
+  });
+  it('return 2A2B given 1243 compared with 1234', () => {
+    let result = main('The system generates 1234 and user inputs 1243.');
+    expect(result).toEqual('2A2B');
+  });
+  it('return 1A1B given 1313 compared with 1234', () => {
+    let result = main('The system generates 1234 and user inputs 1313.');
+    expect(result).toEqual('1A1B');
+  });
 });
